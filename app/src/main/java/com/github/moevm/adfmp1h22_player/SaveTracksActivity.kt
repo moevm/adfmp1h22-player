5e29fe27f6741df1f0de2eccd14b70c2248ed653
Eldar Abibulaev <abibulaev.eldar@gmail.com>
2022-02-28 22:33:46 +0300

    activity_save_tracks.xml namefix

--- app/src/main/java/com/github/moevm/adfmp1h22_player/SaveTracksActivity.kt
+++ app/src/main/java/com/github/moevm/adfmp1h22_player/SaveTracksActivity.kt
@@ -1,15 +1,14 @@
 package com.github.moevm.adfmp1h22_player
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_sava_tracks.*
+import kotlinx.android.synthetic.main.activity_save_tracks.*
 
 class SaveTracksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sava_tracks)
+        setContentView(R.layout.activity_save_tracks)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
@@ -22,7 +21,7 @@ class SaveTracksActivity : AppCompatActivity() {
             tracksList.add(i, track)
         }
 
-        var SaveTrackAdapter = SavaTracksAdapter(tracksList)
+        var SaveTrackAdapter = SaveTracksAdapter(tracksList)
 
         val layoutManager = LinearLayoutManager(this)
         tracksListRV.layoutManager = layoutManager
