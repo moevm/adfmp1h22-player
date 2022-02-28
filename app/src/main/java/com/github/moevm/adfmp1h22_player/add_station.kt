eb4ef51e622d6375e3e9b8c21608390cdada3a39
Eldar Abibulaev <abibulaev.eldar@gmail.com>
2022-02-28 21:38:45 +0300

    Add station file generated

---
+++ app/src/main/java/com/github/moevm/adfmp1h22_player/add_station.kt
@@ -0,0 +1,11 @@
+package com.github.moevm.adfmp1h22_player
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class add_station : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_station)
+    }
+}
