d58b0baf04da9fb01ee70129bed81486420873ea
Eldar Abibulaev <abibulaev.eldar@gmail.com>
2022-02-28 22:31:26 +0300

    StationInfo renamed

--- app/src/main/java/com/github/moevm/adfmp1h22_player/StationInfoActivity.kt
+++ app/src/main/java/com/github/moevm/adfmp1h22_player/StationInfoActivity.kt
@@ -3,7 +3,7 @@ package com.github.moevm.adfmp1h22_player
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class StationInfo : AppCompatActivity() {
+class StationInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_station_info)
