cd26e25eb8906028cf795cf6845d2e806c3bf2c0
Leonid Parfentiev <leo.parfentiev@gmail.com>
2022-04-04 02:02:55 +0300

    Record radio stream into ADTS-encapsulated AAC file
    - Excessive logging
    - Stops working after metadata change
    - Doesn’t clear recordings
    - Too slow

--- app/src/main/java/com/github/moevm/adfmp1h22_player/StreamRecorder.kt
+++ app/src/main/java/com/github/moevm/adfmp1h22_player/StreamRecorder.kt
@@ -15,6 +15,16 @@ import android.media.MediaCodecList
 import android.media.MediaCodec
 import android.media.MediaFormat
 
+fun writeADTSHeader(b: ByteBuffer, framesz: Int) {
+    b.put(0xFF.toByte())
+    b.put(0xF1.toByte())
+    b.put(0x50.toByte())
+    b.put((0x80 or ((framesz shr 11) and 3)).toByte())
+    b.put(((framesz shr 3) and 255).toByte())
+    b.put((0x1F or ((framesz and 7) shl 5)).toByte())
+    b.put(0xFC.toByte())
+}
+
 class StreamRecorder(
     private val handler: Handler,
     private val cb: Callback,
@@ -68,38 +78,45 @@ class StreamRecorder(
         }
 
         if (trackqueue.isEmpty()) {
+            Log.d(TAG, "no next track")
             track = null
         } else {
             val r = trackqueue.remove()
             if (r == null) {
                 doStop()
             } else {
+                Log.d(TAG, "start new track $r")
                 setupRecording(r)
             }
         }
     }
 
     private fun onWriteDone(n: Int) {
-        Log.d(TAG, "onWriteDone")
+        handler.post {
+            Log.d(TAG, "onWriteDone ${chanqueue.size}/${chanfreelist.size}")
 
-        val t = track
-        if (t == null) {
-            Log.w(TAG, "onWriteDone: track is null")
-            return
+            val t = track
+            if (t == null) {
+                Log.w(TAG, "onWriteDone: track is null")
+                return@post
+            }
+            t.pos += n.toLong()
+
+            val b = chanbusyslot
+
+            Log.d(TAG, "WD busy $b")
+
+            if (b == null) {
+                Log.w(TAG, "onWriteDone: busy slot empty")
+                return@post
+            }
+
+            b.clear()
+            chanfreelist.add(b)
+
+            chanbusyslot = null
+            maybeDoOutput()
         }
-        t.pos += n.toLong()
-
-        val b = chanbusyslot
-        if (b == null) {
-            Log.w(TAG, "onWriteDone: busy slot empty")
-            return
-        }
-
-        b.clear()
-        chanfreelist.add(b)
-
-        chanbusyslot = null
-        maybeDoOutput()
     }
 
     private fun onWriteError(exc: Throwable) {
@@ -123,6 +140,8 @@ class StreamRecorder(
     }
 
     private fun maybeDoOutput() {
+        Log.d(TAG, "maybeDoOutput")
+
         if (chanbusyslot != null || chanqueue.isEmpty()) {
             return
         }
@@ -133,22 +152,19 @@ class StreamRecorder(
             return
         }
 
+        Log.d(TAG, "chanqueue/2: ${chanqueue.size}")
+
         // NOTE: not poll() because null is an end flag
         val b = chanqueue.remove()
 
         if (b == null) {
+            Log.d(TAG, "end flag")
+
             // end flag
             startNextTrack()
         } else {
             chanbusyslot = b
-
-            val bs = ByteArray(if (b.remaining() > 4) 4
-                               else b.remaining())
-            b.mark()
-            b.get(bs)
-            b.reset()
-            Log.d(TAG, "t.chan.write (${b.remaining()} at ${t.pos}): ${bs.toList()}}")
-
+            Log.d(TAG, "DO busy $b")
             t.chan.write(b, t.pos, this, ComplHandler())
         }
     }
@@ -215,6 +231,7 @@ class StreamRecorder(
 
                     val n = buf.position()
                     buf.rewind()
+                    buf.limit(n) // TODO: flip instead?
 
                     mc.queueInputBuffer(index, 0, n, ib.timestamp, 0)
                 }
@@ -239,9 +256,9 @@ class StreamRecorder(
                 return
             }
 
-            // TODO: maybe check BUFFER_FLAG_CODEC_CONFIG
-
-            if (info.size == 0) {
+            if (info.flags and MediaCodec.BUFFER_FLAG_CODEC_CONFIG != 0
+                || info.size == 0) {
+                mc.releaseOutputBuffer(index, false)
                 return
             }
 
@@ -251,12 +268,16 @@ class StreamRecorder(
                 return
             }
 
-            val b = getChanBuffer(buf.remaining())
+            val b = getChanBuffer(buf.remaining() + 7)
+            writeADTSHeader(b, buf.remaining() + 7)
             b.put(buf)
 
             mc.releaseOutputBuffer(index, false)
 
             b.flip()
+
+            Log.d(TAG, "onOBA: b=$b")
+
             chanqueue.add(b)
             maybeDoOutput()
         }
@@ -297,7 +318,7 @@ class StreamRecorder(
             val mcn = mcl.findEncoderForFormat(fmt)
             Log.i(TAG, "creating codec name $mcn")
             e = MediaCodec.createByCodecName(mcn)
-            e.setCallback(mediaCodecCallback())
+            e.setCallback(mediaCodecCallback(), handler)
             enc = e
         } else {
             e.stop()
@@ -340,6 +361,7 @@ class StreamRecorder(
     }
 
     private fun doStop() {
+        Log.d(TAG, "doStop")
         cb.onStop()
         enc?.release()
     }
