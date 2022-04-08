d47ea03fcdc6b527404deaf063b1527ad249b0e8
Leonid Parfentiev <leo.parfentiev@gmail.com>
2022-04-08 03:08:30 +0300

    Properly save the save directory URI

--- app/src/main/java/com/github/moevm/adfmp1h22_player/SettingsActivity.kt
+++ app/src/main/java/com/github/moevm/adfmp1h22_player/SettingsActivity.kt
@@ -1,5 +1,6 @@
 package com.github.moevm.adfmp1h22_player
 
+import android.net.Uri
 import android.Manifest
 import android.content.Context
 import android.content.Intent
@@ -16,19 +17,45 @@ import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
+    companion object {
+        const val TAG = "SettingsActivity"
+    }
+
     fun isStoragePermissionGranted(): Boolean {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
                 return true
             } else {
-                Log.d("TAG", "Permission is revoked")
+                Log.d(TAG, "Permission is revoked")
                 ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
                 return false
             }
         } else return true
     }
 
-    var pref : SharedPreferences? = null
+    lateinit var pref: SharedPreferences
+
+    private fun updateSaveFolderButtonName(url: Uri?) {
+        var name: String? = null
+
+        if (url != null) {
+            val path = url.path
+            if (path != null) {
+                val spl = path.split(":")
+                if (spl.size >= 2) {
+                    name = spl[1]
+                }
+            }
+
+            if (name == null) {
+                name = url.toString()
+            }
+        } else {
+            name = resources.getString(R.string.save_folder_not_set)
+        }
+
+        button.setText(name)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,16 +64,23 @@ class SettingsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         pref = getSharedPreferences("Table", Context.MODE_PRIVATE)
-        val countTracks = pref?.getInt("progress", 10)!!
-        val folder = pref?.getString("folder", "Folder")!!
-        button.setText(folder)
+        val countTracks = pref.getInt("progress", 10)
+        val saveurl = pref.getString("folder", null)
+
+        val folder = if (saveurl != null) {
+            Uri.parse(saveurl)
+        } else {
+            null
+        }
+
+        updateSaveFolderButtonName(folder)
 
         seekBar.progress = countTracks
         progressSeekBar.text = seekBar.progress.toString()
         seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 progressSeekBar.text = p0?.progress.toString()
-                pref?.edit()?.putInt("progress", p0?.progress.toString().toInt())?.apply()
+                pref.edit().putInt("progress", p0?.progress.toString().toInt())?.apply()
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {
@@ -59,7 +93,7 @@ class SettingsActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             if(isStoragePermissionGranted()){
-                Log.d("TAG", "OPEN FILES MANAGER")
+                Log.d(TAG, "OPEN FILES MANAGER")
                 var intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
                 startActivityForResult(intent, 1)
             }
@@ -91,12 +125,12 @@ class SettingsActivity : AppCompatActivity() {
 
         when(requestCode){
             1 -> {
-                if(data == null) return
-                // Пока так. Пока не получилось нормально имя папки достать из пути
-                var name = data.data?.path.toString().split(":")[1]
-//                Log.d("TAG", data.data?.path.toString().split(":")[1])
-                pref?.edit()?.putString("folder", name)?.apply()
-                button.text = name.toString()
+                val url = data?.getData()
+                if (url != null) {
+                    Log.d(TAG, "url = $url")
+                    pref.edit().putString("folder", url.toString()).apply()
+                    updateSaveFolderButtonName(url)
+                }
             }
         }
     }
